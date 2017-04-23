c14367f6cf4e796bf4eafa168dbfb2dc0ca50d61
Sravan Kumar Rekandar <sravankumarrekandar@SRAVANs-MacBook-Pro.local>
2017-04-24 02:19:56 +0530

    running app

---
+++ uiApp/data.js
@@ -0,0 +1,103 @@
+/* global window, d3, _
+*/
+
+window.getData = (cb) => {
+  d3.json('movies.json', (error, json) => {
+    let nodes = json
+    nodes.length = 20 // Limiting the set for a quick demo
+    nodes = nodes.filter(e => e.year > 0)
+    const links = []
+    let allYears = _.uniq(nodes.map(e => Number(e.year)))
+    allYears = allYears.filter(e => isFinite(e))
+    allYears = allYears.sort((a, b) => (a - b))
+    const leastYear = Math.min.apply(null, allYears)
+    const maxYear = Math.max.apply(null, allYears)
+
+    /*
+    // Add ten more years to keep the padding
+    for (let i = 1; i <= 10; i += 1) {
+      allYears.push(maxYear + i)
+    }
+    */
+    // Creating Year nodes
+    const yearNodes = allYears.map((e) => {
+      const yearNode = {
+        id: `year-${e}`,
+        title: e.toString(),
+        name: e.toString(),
+        year: e,
+        titleWikiLink: '',
+        directors: [
+        ],
+        direcorWikiLink: '',
+        originals: [],
+        remarks: [],
+        type: 'year-node',
+      }
+      return yearNode
+    })
+    yearNodes.forEach((e, idx) => {
+      if (idx !== 0) {
+        const lastYearNode = yearNodes[idx - 1]
+        e.originals.push(lastYearNode.id)
+      }
+    })
+
+    nodes = [].concat(nodes, yearNodes)
+    nodes.forEach((node, nodeIdx) => {
+      node.name = node.title
+
+      node.originals.forEach((originalNodeId, idx) => {
+        links.push({
+          source: nodes.findIndex(e => e.id === originalNodeId),
+          target: nodeIdx,
+          linkId: `link-${nodeIdx}-${idx}`,
+          value: 1, // A dummy value
+        })
+      })
+
+      if (node.type !== 'year-node') {
+        // Map with previous year node
+
+
+        const yearIndex = allYears.indexOf(Number(node.year))
+        console.log(yearIndex)
+        if (yearIndex > 0 && yearIndex !== allYears.length - 1) {
+          const prevAvailableYear = allYears[yearIndex - 1]
+          const yearNodeIndex = nodes.findIndex(e => e.id === `year-${prevAvailableYear}`)
+          // Mapping to the prev year
+          links.push({
+            source: yearNodeIndex,
+            target: nodeIdx,
+            linkId: `prev-link-${nodeIdx}-year-${node.year}`,
+            type: 'year-link',
+            value: 1, // A dummy value
+          })
+        }
+
+        if (yearIndex < allYears.length - 1 && yearIndex !== -1) {
+          // Mapping to next available year
+          // Mapping to the prev year
+          const nextAvailableYear = allYears[yearIndex + 1]
+          const yearNodeIndex = nodes.findIndex(e => e.id === `year-${nextAvailableYear}`)
+          links.push({
+            source: nodeIdx,
+            target: yearNodeIndex,
+            linkId: `next-link-year-${node.year}-${nodeIdx}`,
+            type: 'year-link',
+            value: 1, // A dummy value
+          })
+        }
+      }
+    })
+
+    cb({
+      nodes,
+      links,
+      leastYear,
+      maxYear,
+      allYears,
+    })
+  })
+}
+
---
+++ uiApp/main.js
@@ -0,0 +1,78 @@
+/*
+global d3, window
+*/
+
+window.getData((data) => {
+  const margin = { top: 1, right: 1, bottom: 6, left: 1 }
+  const width = (data.allYears.length * 100) - margin.left - margin.right
+  const height = 400 - margin.top - margin.bottom
+
+  const formatNumber = d3.format(',.0f')
+  const format = d => `${formatNumber(d)} TWh`
+  const color = d3.scale.category20()
+
+  const svg = d3.select('#chart').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`)
+
+  const sankey = d3.sankey()
+    .nodeWidth(20)
+    .nodePadding(10)
+    .size([width, height])
+
+  const path = sankey.link()
+
+  sankey
+    .nodes(data.nodes)
+    .links(data.links)
+    .layout(32)
+
+  const link = svg.append('g').selectAll('.link')
+    .data(data.links)
+    .enter()
+    .append('path')
+    .attr('class', d => `link ${d.type === 'year-link' ? 'year-link' : ''}`)
+    .attr('d', path)
+    .style('stroke-width', d => (d.type === 'year-link' ? 0 : 1))
+    .sort((a, b) => b.dy - a.dy)
+
+  link.append('title')
+    .text(d => `${d.source.name}  →  ${d.target.name} ${'\n'} ${format(d.value)}`)
+
+  const node = svg.append('g').selectAll('.node')
+    .data(data.nodes)
+    .enter()
+    .append('g')
+    .attr('class', d => `node node-id-${d.id} ${d.type === 'year-node' ? 'year-node' : ''}`)
+    .attr('transform', d => `translate(${d.x},${d.y})`)
+  /*
+      node.call(d3.behavior.drag()
+        .origin(function (d) { return d; })
+        .on('dragstart', function () { this.parentNode.appendChild(this); })
+        .on('drag', dragmove));
+  */
+  node.append('rect')
+    .attr('height', d => (d.dy < 10 ? 10 : d.dy))
+    .attr('width', sankey.nodeWidth())
+    .style('fill', (d) => {
+      d.color = (d.type === 'year-node') ? 'transparent' : color(d.name.replace(/ .*/, ''))
+      return d.color
+    })
+    .style('stroke', d => d3.rgb(d.color).darker(2))
+    .append('title')
+    .text(d => `d.name${'\n'}${format(d.value)}`)
+
+  node.append('text')
+    .attr('x', -6)
+    .attr('y', d => d.dy / 2)
+    .attr('dy', '.35em')
+    .attr('text-anchor', 'end')
+    .attr('transform', null)
+    .text(d => `${d.name} (${d.year})`)
+    .filter(d => d.x < width / 2)
+    .attr('x', 6 + sankey.nodeWidth())
+    .attr('text-anchor', 'start')
+    .attr('class', d => `node-id-${d.id}`)
+})
